164cdc23411a845e8b2c06f9a89368c8b8fad83a
ClaireHien <clairehien.osmont@gmail.com>
2019-10-29 01:16:17 +0100

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,6 @@
 require 'faker'
 
-10.times do 
-    age = rand(7..77)
-    first_name = Faker::Name.first_name
-    User.create!(first_name: first_name, last_name: Faker::Name.last_name, alias: Faker::Creature::Cat.name, age: age, email: Faker::Internet.email(name: first_name), description: Faker::Quote.famous_last_words)
-end
-
-30.times do
-    user = rand(1..10)
+50.times do
+    user = rand(2..11)
     Gossip.create!(title: Faker::Book.title, content: Faker::Quote.famous_last_words, user_id: user)
 end
